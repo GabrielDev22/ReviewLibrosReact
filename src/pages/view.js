bf779f289ef7f8bd90d43bb86f574e13432bb4f1
unknown <gabrieltorresmora2@gmail.com>
2023-02-07 09:11:15 -0300

    creacion de paginas de review de libros leidos por usuarios

---
+++ src/pages/view.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Layout from "../components/layout";
+import { useAppContext } from "../store/store";
+
+export default function View(){
+
+    const [item, setItem] = useState({});
+    const params = useParams();
+    const store = useAppContext();
+
+    useEffect(() => {
+        const book = store.getItem(params.bookId);
+        setItem(book);
+    }, [])
+
+    if(!item){
+        return <Layout>Item Not Found</Layout>
+    }
+
+    return(
+    <Layout>
+        <h2>{item?.title}</h2>
+        <div>{item?.cover? <img src={item.cover} width="400" /> : ''}</div>
+        <div>{item?.author}</div>
+        <div>{item?.intro}</div>
+        <div>{item?.completed ? 'Leido' : 'Por terminar'}</div>
+        <div>{item?.review}</div>
+    </Layout>
+    )
+}
